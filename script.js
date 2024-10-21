a7e1480c4194d54dd0bc8b6b7f19c106e590b241
Rushil <rushiljuneja@gmail.com>
2024-10-21 17:31:46 -0400

    display, next, and previous button functionality has been added

--- script.js
+++ script.js
@@ -12,10 +12,36 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
-
+    const displayStatement = document.getElementById("card-content")
+    if (showingTerm == true){
+        displayStatement.innerHTML = flashcards[currentIndex].term
+    } else {
+        displayStatement.innerHTML = flashcards[currentIndex].definition
+    }  
 }
 
-// The rest of the code you will write is apart of event listeners
+const nextButton = document.getElementById("next-btn")
+const prevButton = document.getElementById("prev-btn")
+
+nextButton.addEventListener("click", function(){
+    if (showingTerm == true){
+        showingTerm = false
+    } else{
+        currentIndex++
+        showingTerm =true
+    }
+    displayCard()
+})
+
+prevButton.addEventListener("click", function(){
+    if (showingTerm == true){
+        showingTerm = false
+        currentIndex--
+    } else{
+        showingTerm =true
+    }
+    displayCard()
+})
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
